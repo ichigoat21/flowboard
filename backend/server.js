f6b2acc5663c40533ffe04d0929f5586a33c5b48
shivansh <shivresides@gmail.com>
2026-02-06 21:41:08 +0530

    backend

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,12 @@
 import { Server } from "socket.io";
 import mongoose from "mongoose";
 import dotenv from "dotenv"
+import { Task } from "./db.js";
 
 dotenv.config()
 
 
-async function db_connection() {
-  await mongoose.connect(`mongodb+srv://shivresides:${process.env.MONGO_DB_PASS}@second-brain.4jq3gmh.mongodb.net/kanban`)
-}
-
-db_connection()
+mongoose.connect(process.env.MONGO_URL)
 
 
 const io = new Server(3001, {
@@ -18,10 +15,43 @@ const io = new Server(3001, {
   },
 });
 
-io.on("connection", (socket) => {
+io.on("connection", async (socket) => {
   console.log("User connected:", socket.id);
 
-  socket.on("message", ()=> {
-    socket.emit("reply", "hi there")
+  const tasks = await Task.find()
+  socket.emit("sync:tasks", tasks)
+
+  socket.on("task:create", async (data)=> {
+    const task = Task.create(data);
+    io.emit("task:created", task)
   });
+
+  socket.on("task:update", async ({ id, updates }) => {
+    const task = await Task.findByIdAndUpdate(
+      id,
+      updates,
+      { new: true }
+    );
+  
+    if (task) {
+      io.emit("task:updated", task);
+    }
+  });
+  
+  socket.on("task:move", async ({ id, column }) => {
+    const task = await Task.findByIdAndUpdate(
+      id,
+      { column },
+      { new: true }
+    );
+  
+    if (task) {
+      io.emit("task:moved", { id, column });
+    }
+  });
+  socket.on("task:delete", async (id) => {
+    await Task.findByIdAndDelete(id);
+    io.emit("task:deleted", id);
+  });
+  
 });
